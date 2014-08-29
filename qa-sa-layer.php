6a6f147e8bf6d160bf9101ed20a0ef2221a1cc61
Amiya Sahu <amiya053@gmail.com>
2014-08-29 19:59:52 +0530

    bugs fixed

--- qa-sa-layer.php
+++ qa-sa-layer.php
@@ -33,27 +33,22 @@
 		{
 			
 			qa_html_theme_base::attribution();
-			/*now get all the answers here and print inside a hidden content*/
+
 			if(qa_opt(qa_sa_admin::PLUGIN_ENABLED)){
+				$this->output('<div id="qa-sa-all-ans" style="display:none;">');
+				$this->output('<div id="ami-sort-util-data" style="display:none;">');
+				$this->output('<div id="sorting-btns">');
+				$this->output('<span style="font-weight:bold;"> '.qa_lang('ami_sa/order_answers_by').' </span> '.
+							'<button class="a_sort_oldest" title="'.qa_lang('ami_sa/oldest_title').'">'.qa_lang('ami_sa/oldest').'</button>'.
+							'<button class="a_sort_newest" title="'.qa_lang('ami_sa/latest_title').'">'.qa_lang('ami_sa/latest').'</button>'.
+							'<button class="a_sort_highest_vote" title="'.qa_lang('ami_sa/highest_votes_title').'">'.qa_lang('ami_sa/highest_votes').'</button>');
+				$this->output('</div>');
 
+				// $this->output('<span id="ans-sort-data" data-page-size="'.qa_opt('page_size_q_as').'"></span>');
+
+				$this->output('</div>');
+				$this->output('</div>');
 			}
-			$this->output('<div id="qa-sa-all-ans" style="display:none;">');
-
-			// require_once AMI_SA_DIR . '/qa-page-question.php' ;
-			// $this->a_list_hidden($qa_content['a_list']);
-			// write some variable properties here 
-			$this->output('<div id="ami-sort-util-data" style="display:none;">');
-			$this->output('<div id="sorting-btns">');
-			$this->output('<span style="font-weight:bold;"> '.qa_lang('ami_sa/order_answers_by').' </span> '.
-						'<button class="a_sort_oldest" title="'.qa_lang('ami_sa/oldest_title').'">'.qa_lang('ami_sa/oldest').'</button>'.
-						'<button class="a_sort_newest" title="'.qa_lang('ami_sa/latest_title').'">'.qa_lang('ami_sa/latest').'</button>'.
-						'<button class="a_sort_highest_vote" title="'.qa_lang('ami_sa/highest_votes_title').'">'.qa_lang('ami_sa/highest_votes').'</button>');
-			$this->output('</div>');
-
-			// $this->output('<span id="ans-sort-data" data-page-size="'.qa_opt('page_size_q_as').'"></span>');
-
-			$this->output('</div>');
-			$this->output('</div>');
 		}
 
 	}
